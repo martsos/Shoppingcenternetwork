e9b3643ceb79c7935f74415a7492c4112bdfe2f1
Soós Martin <martinsoos1001@gmail.com>
2026-03-10 16:36:05 +0100

    Reset2.0

---
+++ database/shema.sql
@@ -0,0 +1,6 @@
+-- In his project, I am attempting to visualize and implement the database schema of a small Hungarian supermarket chain.
+-- Fist step create a database :)
+
+CREATE DATABASE shoppingcenternetwork;
+
+
